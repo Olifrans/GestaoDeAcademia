40b970fea4afccc66c51610466e4feef76e28979
Francisco Oliveira <fransoliveira@gmail.com>
2025-04-05 15:50:16 -0300

    academia

--- src/models/Academia.java
+++ src/models/Academia.java
@@ -1,4 +1,27 @@
 package models;
+import java.util.ArrayList;
 
+//Composição e Polimorfismo
 public class Academia {
+
+    private String nomeAcademia;
+    private ArrayList<Pessoa> pessoas;
+
+    public Academia(String nomeAcademia) {
+        this.nomeAcademia = nomeAcademia;
+        this.pessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(Pessoa pessoa){
+        pessoas.add(pessoa);
+    }
+
+    public  void listarPessoas(){
+        System.out.println("Lista de pessoas da academia");
+        for(Pessoa pessoasDaLista : pessoas){
+            System.out.println(pessoasDaLista.getDescricao());
+        }
+    }
+
+
 }
